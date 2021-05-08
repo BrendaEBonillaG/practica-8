6c7e9056a52963a738b7648ed85db425bcaa8e0a
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-08 14:57:40 -0500

    Ejemplo 2.4 practica 8

--- Ejem2_4.java
+++ Ejem2_4.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Ejem2_4 {
+import java.util.Random;
+
+public class Ejem2_4 {
+
+    public static void main(String[] args){
+        Random randomNumbers = new Random();
+
+        System.err.println(randomNumbers.nextInt());
+        System.err.println(randomNumbers.nextDouble());
+        System.err.println(randomNumbers.nextFloat());
+        System.err.println(randomNumbers.nextLong());
+    }
 }
