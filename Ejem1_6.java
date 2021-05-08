f8808e260462a22d9eec4736932bed3f1532a4da
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-05-08 14:20:46 -0500

    Ejemplo 1.6 practica 8

---
+++ Ejem1_6.java
@@ -0,0 +1,17 @@
+public class Ejem1_6 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-6.6));
+        System.out.println(Math.max(20, 30));
+        System.out.println(Math.min(200, 300));
+        System.out.println(Math.pow(6, 3));
+        System.out.println(Math.sqrt(26));
+        System.out.println(Math.ceil(7.6));
+        System.out.println(Math.floor(5.9));
+        System.out.println(Math.exp(4));
+        System.out.println(Math.log(40));
+        System.out.println(Math.toDegrees(4));
+
+    }
+}
